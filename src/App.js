5e1e950349de2636e2bdf8261199da1b11090e57
liangliew <liewenleong07@163.com>
2025-05-14 12:36:51 +0800

    nextjs changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+import logo from '../src/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to React</h1>
+        <img src={logo.src} className="App-logo" alt="logo" />
+        <h1>Welcome to React!!!</h1>
       </header>
     </div>
   );
